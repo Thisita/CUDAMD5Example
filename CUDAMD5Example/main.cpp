157490efab834b653190ab32db02a4aa4333c10c
Ian Ledrick <thisita@outlook.com>
2014-04-28 19:20:55 -0400

    Add gpu bench and remove unneeded code

--- CUDAMD5Example/main.cpp
+++ CUDAMD5Example/main.cpp
@@ -35,17 +35,29 @@ int main(int argc, char **argv) {
     }
  
     len = strlen(msg);
- 
-    // benchmark
+	
+	printf("Testing CPU:\n\n");
+    // benchmark cpu
     for (i = 0; i < 100; i++) {
         md5((uint8_t*)msg, len, result);
     }
  
-    // display result
+    // display result cpu
     for (i = 0; i < 16; i++)
         printf("%2.2x", result[i]);
     puts("");
-    
+
+	printf("\n\nTesting GPU\n\n");
+    // benchmark gpu
+    for (i = 0; i < 100; i++) {
+		md5WithCuda((uint8_t*)msg, len, result);
+    }
+ 
+    // display result cpu
+    for (i = 0; i < 16; i++)
+        printf("%2.2x", result[i]);
+    puts("");
+
     // cudaDeviceReset must be called before exiting in order for profiling and
     // tracing tools such as Nsight and Visual Profiler to show complete traces.
     cudaStatus = cudaDeviceReset();
@@ -56,35 +68,3 @@ int main(int argc, char **argv) {
     
     return 0;
 }
-
-
-/** original main **/
-#if 0
-int main()
-{
-    const int arraySize = 5;
-    const int a[arraySize] = { 1, 2, 3, 4, 5 };
-    const int b[arraySize] = { 10, 20, 30, 40, 50 };
-    int c[arraySize] = { 0 };
-
-    // Add vectors in parallel.
-    cudaError_t cudaStatus = md5WithCuda(c, a, b, arraySize);
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "addWithCuda failed!");
-        return 1;
-    }
-
-    printf("{1,2,3,4,5} + {10,20,30,40,50} = {%d,%d,%d,%d,%d}\n",
-        c[0], c[1], c[2], c[3], c[4]);
-
-    // cudaDeviceReset must be called before exiting in order for profiling and
-    // tracing tools such as Nsight and Visual Profiler to show complete traces.
-    cudaStatus = cudaDeviceReset();
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaDeviceReset failed!");
-        return 1;
-    }
-
-    return 0;
-}
-#endif
