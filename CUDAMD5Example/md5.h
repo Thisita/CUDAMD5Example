ed0907835d9465fcdd49c97f7811dd0fc2287944
Ian Zachary Ledrick <thisita@outlook.com>
2014-04-28 18:03:12 -0400

    Licensing

---
+++ CUDAMD5Example/md5.h
@@ -0,0 +1,19 @@
+/*
+    CUDAMD5Example
+    Copyright © 2014 Ian Zachary Ledrick, also known as Thisita.
+    
+    This file is part of CUDAMD5Example.
+
+    CUDAMD5Example is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    CUDAMD5Example is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with CUDAMD5Example.  If not, see <http://www.gnu.org/licenses/>.
+*/
